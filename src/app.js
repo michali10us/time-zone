6fd5925dfd06d2cb19ce6814c677be6217047a37
michal mechlovich <michali10us@gmail.com>
2023-06-01 12:19:58 -0700

    function updateTime()

--- src/app.js
+++ src/app.js
@@ -1,40 +1,65 @@
-function showSelctedCountry(event) {
-  if (event.target.value.length > 0) {
-    let now = moment().tz(event.target.value).format("dddd, MMMM DD, YYYY LT");
-    alert(`It is ${now} in ${event.target.value}`);
+function updateTime() {
+  let newyorkUpdate = document.querySelector("#new-york .lastUpdate");
+  if (newyorkUpdate) {
+    let newyorkElement = moment().tz("America/New_York");
+    newyorkUpdate.innerHTML = newyorkElement.format("MMM DD, YYYY");
+    let newyorkTime = document.querySelector("#newyork-time");
+    newyorkTime.innerHTML = newyorkElement.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
   }
+
+  let romeUpdate = document.querySelector("#rome .lastUpdate");
+  if (romeUpdate) {
+    let romeElement = moment().tz("Europe/Rome");
+    romeUpdate.innerHTML = romeElement.format("MMM DD, YYYY");
+    let romeTime = document.querySelector("#rome-time");
+    romeTime.innerHTML = romeElement.format("hh:mm:ss [<small>]A[</small>]");
+  }
+
+  let canadaUpdate = document.querySelector("#canada .lastUpdate");
+  if (canadaUpdate) {
+    let canadaElement = moment().tz("America/Vancouver");
+    canadaUpdate.innerHTML = canadaElement.format("MMM DD, YYYY");
+    let canadaTime = document.querySelector("#canada-time");
+    canadaTime.innerHTML = canadaElement.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let SydneyUpdate = document.querySelector("#Sydney .lastUpdate");
+  if (SydneyUpdate) {
+    let SydneyElement = moment().tz("Australia/Sydney");
+    SydneyUpdate.innerHTML = SydneyElement.format("MMM DD, YYYY");
+    let SydneyTime = document.querySelector("#Sydney-time");
+    SydneyTime.innerHTML = SydneyElement.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function showSelctedCountry(event) {
+  let cityName = event.target.value;
+  let cityElement = document.querySelector(".citiesContainer");
+  let cityTime = moment().tz(cityName);
+  cityElement.innerHTML = `
+    <div class="citiesContainer">
+        <div class="container text-center"></div>
+        <div class="row">
+          <div class="col-8">
+            <div class="city" >
+              ${cityName} <br />
+              <span class="lastUpdate">${cityTime.format("MMM DD,YYYY")}</span>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="time" >${cityTime.format("hh:mm:ss A")}</div>
+          </div>
+          </div>`;
+  updateTime();
 }
 
 let countrieSelect = document.querySelector("#countries");
 countrieSelect.addEventListener("change", showSelctedCountry);
-
-function updateTime() {
-  let newyorkUpdate = document.querySelector("#new-york .lastUpdate");
-  let newyorkElement = moment().tz("America/New_York");
-  newyorkUpdate.innerHTML = newyorkElement.format("MMM DD, YYYY");
-  let newyorkTime = document.querySelector("#newyork-time");
-  newyorkTime.innerHTML = newyorkElement.format(
-    "hh:mm:ss [<small>]A[</small>]"
-  );
-
-  let romeUpdate = document.querySelector("#rome .lastUpdate");
-  let romeElement = moment().tz("Europe/Rome");
-  romeUpdate.innerHTML = romeElement.format("MMM DD, YYYY");
-  let romeTime = document.querySelector("#rome-time");
-  romeTime.innerHTML = romeElement.format("hh:mm:ss [<small>]A[</small>]");
-
-  let canadaUpdate = document.querySelector("#canada .lastUpdate");
-  let canadaElement = moment().tz("America / Vancouver");
-  canadaUpdate.innerHTML = canadaElement.format("MMM DD, YYYY");
-  let canadaTime = document.querySelector("#canada-time");
-  canadaTime.innerHTML = canadaElement.format("hh:mm:ss [<small>]A[</small>]");
-
-  let SydneyUpdate = document.querySelector("#Sydney .lastUpdate");
-  let SydneyElement = moment().tz("Australia/Sydney");
-  SydneyUpdate.innerHTML = SydneyElement.format("MMM DD, YYYY");
-  let SydneyTime = document.querySelector("#Sydney-time");
-  SydneyTime.innerHTML = SydneyElement.format("hh:mm:ss [<small>]A[</small>]");
-}
-
 updateTime();
 setInterval(updateTime, 1000);
